80a471648ea7c3d5aa013fd0309b031a16652b9c
Manuel Moya <mmoyam@gmail.com>
2016-11-22 23:49:23 -0300

    add descriptión
    add descriptión

--- dateconvert.erl
+++ dateconvert.erl
@@ -1,3 +1,8 @@
+
+% call 1: (dateconvert:new_date_dmy({1,0,0}))(suma,{2016,12,31}). 
+% call 2: FUN1 = dateconvert:new_date_dmy({1,0,0}).   
+% 		  FUN1(suma,{2016,12,31}). 
+
 -module(dateconvert).
 -export([new_date/3, new_date_dmy/1]).
 
